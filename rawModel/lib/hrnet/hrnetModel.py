4dbe6508df1b5e88ba79a312ffa205e09455e18a
FMS <30920211154173@stu.xmu.edu.cn>
2023-08-30 17:30:53 +0800

    pth_version

--- rawModel/lib/hrnet/hrnetModel.py
+++ rawModel/lib/hrnet/hrnetModel.py
@@ -143,7 +143,6 @@ class HRnetModelPrediction:
             inputs, origin_img, center, scale = PreProcess(frame, track_bboxs, cfg, num_peroson)
 
             inputs = inputs[:, [2, 1, 0]]
-
             if torch.cuda.is_available():
                 inputs = inputs.cuda()
             #-------导入onnx进行预测-----
